0d96f7e025e140c94e83c95186008e9335c98ca3
Alina <AliKochneva@yandex.ru>
2022-12-20 16:27:33 +0700

    Ex_2

--- Homework/Seminar_3/Ex_2.py
+++ Homework/Seminar_3/Ex_2.py
@@ -5,3 +5,13 @@
 - [2, 3, 4, 5, 6] => [12, 15, 16];
 - [2, 3, 5, 6] => [12, 15] '''
 
+import random
+
+n = int(input('Введите количество элиментов списка: '))
+
+
+numbers = list(random.randint(1, 10) for _ in range(n))
+
+print(f'Первоначальный список: {numbers}')
+result = [numbers[i] * numbers[len(numbers) - 1 - i] for i in range(int(len(numbers)/2))]
+print(f'Произведение пар: {result}')
