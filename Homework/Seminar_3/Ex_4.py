8b681cc8feeff9cf5599eb80ef8bade9f380d383
Alina <AliKochneva@yandex.ru>
2022-12-20 14:58:57 +0700

    Ex_4

--- Homework/Seminar_3/Ex_4.py
+++ Homework/Seminar_3/Ex_4.py
@@ -5,3 +5,13 @@
 - 45 -> 101101
 - 3 -> 11
 - 2 -> 10'''
+
+def to_binary(number):
+    result = ''
+    while number > 0:
+        result = str(number % 2) + result
+        number = number // 2
+    return result
+    
+number = int(input('Введите целое число: '))
+print(to_binary(number))
